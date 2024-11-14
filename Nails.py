bfc9a7024c9bc67210518e935b47bd6d78129551
Irina Galaktionova <irgaled@mail.ru>
2024-11-14 15:29:16 +0300

    Add files via upload

---
+++ Nails.py
@@ -0,0 +1,26 @@
+Nail_style = ["Шеллак", "Френч", "Обычный лак", "Гель-лак", "Акрил"]
+Price = [2000, 1500, 1000, 3000, 3500]
+Week = [4, 5, 4, 8, 6]
+
+
+total_visits = sum(Week)  
+
+average_visits = total_visits / 7  
+
+total_revenue = sum(price * visits for price, visits in zip(Price, Week))   
+
+revenue_per_style = {style: price * visits for style, price, visits in zip(Nail_style, Price, Week)}
+
+
+average_revenue_per_style = {style: revenue / 7 for style, revenue in revenue_per_style.items()}
+
+
+print(f"Среднее значение посещений салона в день: {average_visits:.2f}")
+print(f"Общее количество посещений салона: {total_visits}")
+print(f"Выручка салона: {total_revenue} руб.")
+print("Выручка по видам маникюра:")
+for style, revenue in revenue_per_style.items():
+    print(f"{style}: {revenue} руб.")
+print("Средняя выручка в день по видам маникюра:")
+for style, avg_revenue in average_revenue_per_style.items():
+    print(f"{style}: {avg_revenue:.2f} руб.")
